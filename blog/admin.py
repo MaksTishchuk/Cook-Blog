5e45ff815001680c0b10eef136e79e814ff07128
Maks Tishchuk <makstischuk@gmail.com>
2021-08-07 18:10:47 +0300

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
+from . import models
+
+
+class RecipeInLIne(admin.StackedInline):
+    model = models.Recipe
+    extra = 1
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'title', 'category', 'create_at']
+    inlines = [RecipeInLIne]
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'post', 'prep_time', 'cook_time']
+
+
+# MPTTModelAdmin позволяет сделать сдвиг вправо в админке для подкатегорий для наглядности
+admin.site.register(models.Category, MPTTModelAdmin)
+admin.site.register(models.Tag)
+admin.site.register(models.Comment)
